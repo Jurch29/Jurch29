21ae70c4c607bd0b5ef56c85961d34f21fc9878e
Jurch29 <julien.rch.29@gmail.com>
2020-04-21 20:50:28 +0200

    Clean sans comment

--- test.c
+++ test.c
@@ -52,8 +52,8 @@ void test_createString()
 void test_sizeString()
 {
     printf("sizeString() : ");
-
     char *string = createString("ceci est une chaine de 37 caracteres.");
+
     if (string==NULL){
         printf("\033[31;1m Fail \033[0m (NULL value) \n");
         nb_funcTestValid--;
@@ -76,30 +76,149 @@ void test_copyString()
 {
     printf("copyString() : ");
     char *stringCopied;
-    copyString(stringCopied,"le message a copier");
+    char *stringToCopy = "le message a copier";
+
+    copyString(stringCopied,stringToCopy);
+    if (stringCopied==NULL){
+        printf("\033[31;1m Fail \033[0m (NULL value) \n");
+        nb_funcTestValid--;
+    }
+    else
+    {
+        if (strcmp(stringCopied,stringToCopy)==0)
+        {
+            printf("\033[32;1m Success \033[0m\n");
+        }
+        else
+        {
+            printf("\033[31;1m Fail \033[0m\n");
+            nb_funcTestValid--;
+        }
+    }
 }
 
 void test_concatString()
 {
     printf("concatString() : ");
+    char *string1 = "il était ";
+    char *string2 = "une fois";
+    char *finalString;
+
+    finalString = concatString(string1,string2);
+    if (finalString==NULL){
+        printf("\033[31;1m Fail \033[0m (NULL value) \n");
+        nb_funcTestValid--;
+    }
+    else
+    {
+        if (strcmp(finalString,strcpy(string1,string2))==0)
+        {
+            printf("\033[32;1m Success \033[0m\n");
+        }
+        else
+        {
+            printf("\033[31;1m Fail \033[0m\n");
+            nb_funcTestValid--;
+        }
+    }
 }
 
 void test_compareString()
 {
     printf("compareString() : ");
+    char *string1 = "je suis *";
+    char *string2 = "je suis";
+
+    int comparator = compareString(string1,string1);
+    if (comparator==NULL){
+        printf("\033[31;1m Fail \033[0m (NULL value) \n");
+        nb_funcTestValid--;
+    }
+    else
+    {
+        if (!comparator)
+        {
+            printf("\033[32;1m Success \033[0m\n");
+        }
+        else
+        {
+            printf("\033[31;1m Fail \033[0m\n");
+            nb_funcTestValid--;
+        }
+    }
 }
 
 void test_isCaracInString()
 {
     printf("isCaracInString() : ");
+    char *string1 = "le petit navire est géniale ?!?";
+    char carac = '?';
+
+    int positionChar = isCaracInString(carac, string1);
+    if (positionChar==NULL){
+        printf("\033[31;1m Fail \033[0m (NULL value) \n");
+        nb_funcTestValid--;
+    }
+    else
+    {
+        if (positionChar==28)
+        {
+            printf("\033[32;1m Success \033[0m\n");
+        }
+        else
+        {
+            printf("\033[31;1m Fail \033[0m\n");
+            nb_funcTestValid--;
+        }
+    }
 }
 
 void test_countCaracInString()
 {
     printf("countCaracInString() : ");
+    char *string1 = "le petit navire est géniale ?!?";
+    char carac = 'e';
+
+    int countOccurence = countCaracInString(carac,string1);
+    if (countOccurence==NULL){
+        printf("\033[31;1m Fail \033[0m (NULL value) \n");
+        nb_funcTestValid--;
+    }
+    else
+    {
+        if (countOccurence==5)
+        {
+            printf("\033[32;1m Success \033[0m\n");
+        }
+        else
+        {
+            printf("\033[31;1m Fail \033[0m\n");
+            nb_funcTestValid--;
+        }
+    }
 }
 
 void test_searchStringInString()
 {
     printf("searchStringInString() : ");
+    char *string1 = "je suis content!::.";
+    char *string2 = "je suis con";
+
+    int isStrInStr = searchStringInString(string1,string2);
+    if (isStrInStr==NULL){
+        printf("\033[31;1m Fail \033[0m (NULL value) \n");
+        nb_funcTestValid--;
+    }
+    else
+    {
+        if (isStrInStr==1)
+        {
+            printf("\033[32;1m Success \033[0m\n");
+        }
+        else
+        {
+            printf("\033[31;1m Fail \033[0m\n");
+            nb_funcTestValid--;
+        }
+    }
 }
