543e5f8f594ad3133ae61186af8df63b483f8dd8
Fantomas29 <rochard.johan@gmail.com>
2020-09-14 07:47:13 +0200

    rien ne fonctionne

--- strFunc.c
+++ strFunc.c
@@ -49,14 +49,15 @@ void copyString(char *stringCopied,char *stringToCopy)
      char caractereActuel=0;
     do
     {   
-        caractereActuel=stringToCopy[nombreDeCarateres];
-        nombreDeCarateres++;
+        caractereActuel=stringToCopy[nombreDeCaracteres];
+        nombreDeCaracteres++;
     } 
     while (caractereActuel!='\0');
-    nombreDeCarateres--;
-    for(i=0;i<=nombreDeCarateres;++i)
+    nombreDeCaracteres--;
+    // int stringCopied[nombreDeCaracteres];
+    for(i=0;i<=nombreDeCaracteres;++i)
     {
-        stringToCopy[i]=stringCopied[i];
+        stringCopied[i]=stringToCopy[i];
     }
     /*Copie stringToCopy dans stringCopied*/
     // à implémenter
@@ -64,31 +65,160 @@ void copyString(char *stringCopied,char *stringToCopy)
 
 char *concatString(char *string1,char *string2)
 {
+    int nombreDeCarateres1=0;
+    char caractereActuel=0;
+    //on compte le nombre de caractere dans string1 et string2
+    /*----------------------------*/
+    do
+    {   
+        caractereActuel=string1[nombreDeCarateres1];
+        nombreDeCarateres1++;
+    } 
+    while (caractereActuel!='\0');
+    nombreDeCarateres1--;
+    int nombreDeCarateres2=0;
+    char caractereActuel=0;
+    do
+    {   
+        caractereActuel=string2[nombreDeCarateres2];
+        nombreDeCarateres2++;
+    } 
+    while (caractereActuel!='\0');
+    nombreDeCarateres2--;
+    /*---------------------------*/
+    //une fois compte, on incremente string 2 dans string 1 a partir du dernier caractere de string 1
+    int a=0,i=0;
+    for(i=nombreDeCarateres1+1;i<=nombreDeCarateres2+1;i++)
+    {
+        string1[i]=string2[a];
+        a=a+1;
+    }
     
     /*Ajoute le contenu de string2 à la suite de string1*/
     // à implémenter
-    return NULL;
+    return *string1;
 }
 
 int compareString(char *string1,char *string2)
 {
+    int a=1;
+    // if (strmp(string1,string2)==0)
+    // {
+    //     a=0;
+    // }
+     
     /*Compare string1 et string2, renvoie 0 si diffèrente et 1 si égale*/
     // à implémenter
-    return NULL;
+    return a;
 }
 
 int isCaracInString(char carac,char *string)
-{
+{   int i=0,nombreDeCaracteres=0,caractereActuel=0,a=-1;
+    do
+    {
+        caractereActuel=string[nombreDeCaracteres];
+        nombreDeCaracteres+=1;
+    } while (caractereActuel!='\0');
+        nombreDeCaracteres--;
+        for(i=0;i<=nombreDeCaracteres;++i) 
+        {
+            if(string[i]==carac)
+            {
+                a=i;
+                i=nombreDeCaracteres+1;
+            }
+        }
     /*Recherche carac dans string, si il est présent il renvoie l'index de la première occurence sinon -1*/
     // à implémenter
-    return NULL;
+    return a;
 }
 
 int countCaracInString(char carac,char *string)
+{    int i=0,nombreDeCaracteres=0;
+     char caractereActuel=0;
+    do
+    {   
+        caractereActuel=stringToCopy[nombreDeCaracteres];
+        nombreDeCaracteres++;
+    } 
+    while (caractereActuel!='\0');
+    nombreDeCaracteres--;
+    // int stringCopied[nombreDeCaracteres];
+    for(i=0;i<=nombreDeCaracteres;++i)
+    {
+        stringCopied[i]=stringToCopy[i];
+    }
+    /*Copie stringToCopy dans stringCopied*/
+    // à implémenter
+}
+
+char *concatString(char *string1,char *string2)
 {
+    int nombreDeCarateres1=0;
+    char caractereActuel=0;
+    //on compte le nombre de caractere dans string1 et string2
+    /*----------------------------*/
+    do
+    {   
+        caractereActuel=string1[nombreDeCarateres1];
+        nombreDeCarateres1++;
+    } 
+    while (caractereActuel!='\0');
+    nombreDeCarateres1--;
+    int nombreDeCarateres2=0;
+    char caractereActuel=0;
+    do
+    {   
+        caractereActuel=string2[nombreDeCarateres2];
+        nombreDeCarateres2++;
+    } 
+    while (caractereActuel!='\0');
+    nombreDeCarateres2--;
+    /*---------------------------*/
+    //une fois compte, on incremente string 2 dans string 1 a partir du dernier caractere de string 1
+    int a=0,i=0;
+    for(i=nombreDeCarateres1+1;i<=nombreDeCarateres2+1;i++)
+    {
+        string1[i]=string2[a];
+        a=a+1;
+    }
+    
+    /*Ajoute le contenu de string2 à la suite de string1*/
+    // à implémenter
+    return *string1;
+}
+
+int compareString(char *string1,char *string2)
+{
+    int a=1;
+    // if (strmp(string1,string2)==0)
+    // {
+    //     a=0;
+    // }
+     
+    /*Compare string1 et string2, renvoie 0 si diffèrente et 1 si égale*/
+    // à implémenter
+    return a;
+}
+
+int isCaracInString(char carac,char *string)
+{   int i=0,nombreDeCaracteres=0,caractereActuel=0,a=-1;
+    do
+    {
+        caractereActuel=string[nombreDeCaracteres];
+        nombreDeCaracteres+=1;
+    } while (caractereActuel!='\0');
+        nombreDeCaracteres--;
+        for(i=0;i<=nombreDeCaracteres;++i) 
+        {
+            if(string[i]==carac)
+            {
+                a+=1;
+            }
+        }
     /*Compte le nombre d'occurence de carac dans string et renvoie le résultat*/
     // à implémenter
-    return NULL;
+    return a;
 }
 
 int searchStringInString(char *string1,char *string2)
